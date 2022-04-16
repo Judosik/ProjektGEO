24a8addc89bca9f1eef84a356e678b320756a2be
Judosik <01160114@pw.edu.pl>
2022-04-16 18:38:48 +0200

    Poprawki w wywolaniu brakujacych funkcji

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ Created on Sat Apr 16 15:05:26 2022
 
 @author: Misiek
 """
-
 import pandas as pd
 import numpy as np
 from klasa import Transformacje
@@ -34,11 +33,10 @@ def wys(lista = list, nazwa = 'wsp', jedn = 'm'):
 
 
 
-
 # utworzenie obiektu
 geo = Transformacje(model = "wgs84")
 # dane XYZ geocentryczne
-xyz_sr = [3664940.500, 1409153.590, 5009571.170]
+xyz_pkt = [3664940.500, 1409153.590, 5009571.170]
 
 file = 'wsp_inp.txt'
 
@@ -52,19 +50,30 @@ tab_wsp00 = []
 tab_wsp92 = []
 tab_wspneu = []
 
-for xyz in dane:
+
+plh_pkt = geo.hirvonen(xyz_pkt)
+
+pozycje = geo.pozycje(plh_pkt, dane, maska = -5)
+
+for i, xyz in enumerate(dane):
 
     plh = geo.hirvonen(xyz)
     xyzk = geo.plh2XYZ(plh)
     wsp00 = geo.u2000(plh)
     wsp92 = geo.u1992(plh)
-    wspneu = geo.neu(xyz,xyz_sr)
+    wspneu = geo.neu(xyz,xyz_pkt)
+    odl2d = geo.odl2D(xyz, xyz_pkt)
+    odl3d = geo.odl3D(xyz, xyz_pkt)
+    
+    
     
     wys(plh,'plh  ','st')
     wys(xyzk,'xyz  ')
     wys(wsp00,'wsp00')
     wys(wsp92,'wsp92')
     wys(wspneu,'NEU  ')
+    print(f'el,Az : {pozycje[i,0] :.5f},  {pozycje[i,1] :.5f}')
+    print(f'2D,3D : {odl2d : .3f}, {odl3d : .3f}')
     
     print('\n')
     
@@ -74,12 +83,15 @@ for xyz in dane:
     tab_wsp00.append(wsp00)
     tab_wsp92.append(wsp92)
     tab_wspneu.append(list(wspneu))
+
     
 tab_plh = np.array(tab_plh)
 tab_xyzk = np.array(tab_xyzk)
 tab_wsp00 = np.array(tab_wsp00)
 tab_wsp92 = np.array(tab_wsp92)
 tab_wspneu = np.array(tab_wspneu)
+
+
 
 
 
